69ac0ebe244a463ef388e46ba0b98b90b543ec93
Deepraj-devikar <deeprajdevikar19@gmail.com>
2023-01-17 19:03:42 +0530

    REFACTOR: user service for code simplicity

--- FunDoNotes/src/services/user.service.js
+++ FunDoNotes/src/services/user.service.js
@@ -2,13 +2,19 @@ import User from '../models/user.model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { sendMail } from '../utils/mail.util';
+import HttpStatus from 'http-status-codes';
+
+//password hashing
+const getHashPassword = async (password) => {
+	const salt = await bcrypt.genSalt(process.env.SALT_ROUND);
+	const hashedPassword = await bcrypt.hash(password, salt);
+	return hashedPassword;
+}
 
 //register user
 export const registerUser = async (body) => {
 	// password hashing before saving to database
-	const salt = await bcrypt.genSalt(process.env.SALT_ROUND);
-	const hashedPassword = await bcrypt.hash(body.password, salt);
-	body.password = hashedPassword;
+	body.password = await getHashPassword(body.password);
 	const data = await User.create(body);
 	return data;
 };
@@ -17,14 +23,14 @@ export const registerUser = async (body) => {
 export const loginUser = async (body) => {
 	const user = await User.findOne({email: body.email});
 	if(!user){
-		return {error: 1, status: 404, message: "User Not found."};
+		return {error: 1, status: HttpStatus.NOT_FOUND, message: "User Not found."};
 	}
 	const isMatchedPassword = await bcrypt.compare(body.password, user.password);
 	if (!isMatchedPassword) {
-		return {error: 1, status: 401, message: "Invalid Password!"};
+		return {error: 1, status: HttpStatus.UNAUTHORIZED, message: "Invalid Password!"};
 	}
 	const token = jwt.sign({id: user.id, email: user.email}, process.env.AUTH_SECRET_KEY);
-	return {error: 0, status: 200, ok: 'ok', user: user, token: token, message: "Login successfull"};
+	return {error: 0, status: HttpStatus.OK, ok: 'ok', user: user, token: token, message: "Login successfull"};
 };
 
 //forget password
@@ -32,7 +38,7 @@ export const forgetPassword = async (email) => {
 	console.log("INPUT - user.service -> forgetPassword ----->", email);
 	const user = await User.findOne({email: email});
 	if(!user){
-		return {error: 1, status: 404, message: "User Not found."};
+		return {error: 1, status: HttpStatus.NOT_FOUND, message: "User Not found."};
 	}
 	const token = jwt.sign({id: user.id, email: user.email}, process.env.AUTH_SECRET_KEY+user.password, {expiresIn: '15m'});
 	const resetPasswordUrl = process.env.BASE_URL + `/users/reset_password/${user.email}/${token}`;
@@ -42,7 +48,7 @@ export const forgetPassword = async (email) => {
 		text: "your password reset link :- " + resetPasswordUrl,
 		html: "<h1>your password reset link</h1><br><p>"+resetPasswordUrl+"</p>"
 	});
-	return {error: 0, status: 200, ok: 'ok', message: "Password reset link is send to your email."};	
+	return {error: 0, status: HttpStatus.OK, ok: 'ok', message: "Password reset link is send to your email."};	
 }
 
 //reset password
@@ -50,18 +56,16 @@ export const resetPassword = async (params, body) => {
 	console.log("INPUT - user.service -> resetPassword ----->", params._email);
 	const user = await User.findOne({email: params._email});
 	if(!user){
-		return {error: 1, status: 404, message: "User Not found."};
+		return {error: 1, status: HttpStatus.NOT_FOUND, message: "User Not found."};
 	}
-	const forgetPasswordUser = jwt.verify(params._token, process.env.AUTH_SECRET_KEY+user.password);
+	const forgetPasswordUser = await jwt.verify(params._token, process.env.AUTH_SECRET_KEY+user.password);
 	if (forgetPasswordUser.id != user.id || forgetPasswordUser.email != user.email) {
-		return {error: 1, status: 401, message: "Password reset token invalid or expired."};
+		return {error: 1, status: HttpStatus.UNAUTHORIZED, message: "Password reset token invalid or expired."};
 	}
 	if (body.password != body.confirm_password) {
-		return {error: 1, status: 403, message: "Password and confirm password is not match."};
+		return {error: 1, status: HttpStatus.FORBIDDEN, message: "Password and confirm password is not match."};
 	}
-	const salt = await bcrypt.genSalt(process.env.SALT_ROUND);
-	const hashedPassword = await bcrypt.hash(body.password, salt);
-	
+	const hashedPassword = await getHashPassword(body.password);
 	const data = await User.findByIdAndUpdate(
         {
             _id: user.id,
@@ -80,5 +84,5 @@ export const resetPassword = async (params, body) => {
 		text: "Your password reset successfully for FunDoNotes.",
 		html: "<h1>Your password reset successfully for FunDoNotes.</h1>"
 	});
-	return {error: 0, status: 200, ok: 'ok', user: data, message: "Password reset successfully."};	
+	return {error: 0, status: HttpStatus.OK, ok: 'ok', user: data, message: "Password reset successfully."};	
 }
