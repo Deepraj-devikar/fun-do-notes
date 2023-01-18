4d56d8e6be429bda71077125e13f788490f64188
Deepraj-devikar <deeprajdevikar19@gmail.com>
2023-01-18 22:49:18 +0530

    ADDED: forget password integration tests

--- FunDoNotes/tests/integration/user.test.js
+++ FunDoNotes/tests/integration/user.test.js
@@ -416,4 +416,81 @@ describe('User APIs Test', () => {
 				});
 		});
 	});
+
+	/**
+	 * Test forget password route
+	 * - should response as send link in mail
+	 * - should response email is required
+	 * - should response needs valid email
+	 * - should response user not found 
+	 */
+	describe('POST /users/forget-password', () => {
+		it('should forget passowrd and return response as send link in mail', (done) => {
+			const user = {
+				email: "pratik@gmail.com"
+			};
+			request(app)
+				.post('/api/v1/users/forget-password')
+				.send(user)
+				.end((err, res) => {
+					expect(res.statusCode).to.be.equal(200);
+					expect(res.body).to.be.an('object');
+					expect(res.body.error).to.be.an("number");
+					expect(res.body.error).to.be.equal(0);
+					expect(res.body.message).to.be.a('string');
+					expect(res.body.message).to.be.equal("Password reset link is send to your email.");
+					done();
+				});
+		});
+
+		it('should forget passowrd and return response as send link in mail', (done) => {
+			request(app)
+				.post('/api/v1/users/forget-password')
+				.end((err, res) => {
+					expect(res.statusCode).to.be.equal(500);
+					expect(res.body).to.be.an('object');
+					expect(res.body.message).to.be.an('string');
+					expect(res.body.message).to.be.equal("\"email\" is required");
+					expect(res.body.data).to.be.an('string').to.be.equal("");
+					expect(res.body.data).to.be.equal("");
+					done();
+				});
+		});
+
+		it('should forget password and return error for email format', (done) => {
+			const user = {
+				email: "pratik"
+			};
+			request(app)
+				.post('/api/v1/users/forget-password')
+				.send(user)
+				.end((err, res) => {
+					expect(res.statusCode).to.be.equal(500);
+					expect(res.body).to.be.an('object');
+					expect(res.body.message).to.be.an('string');
+					expect(res.body.message).to.be.equal( "\"email\" must be a valid email");
+					expect(res.body.data).to.be.an('string').to.be.equal("");
+					expect(res.body.data).to.be.equal("");
+					done();
+				});
+		});
+
+		it('should forget password return error for user not found', (done) => {
+			const user = {
+				email: "pratik1@gmail.com"
+			};
+			request(app)
+				.post('/api/v1/users/forget-password')
+				.send(user)
+				.end((err, res) => {
+					expect(res.statusCode).to.be.equal(404);
+					expect(res.body).to.be.an('object');
+					expect(res.body.error).to.be.an("number");
+					expect(res.body.error).to.be.equal(1);
+					expect(res.body.message).to.be.a('string');
+					expect(res.body.message).to.be.equal("User Not found.");
+					done();
+				});
+		});
+	});	
 });
